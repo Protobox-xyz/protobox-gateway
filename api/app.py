0edbabe1c2dda9e1d265746db5b369167bdaf961
sosoxuc <ioseb.khutsishvili@gmail.com>
2023-02-25 10:58:07 -0700

    Add Nginx

--- api/app.py
+++ api/app.py
@@ -4,12 +4,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+api = FastAPI(title="DLS API")
+app.mount("/api", api)
 
-@app.get("/")
+
+@api.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
