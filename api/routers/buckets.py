283c5b1414e9008781da19805a830aaa645749f1
sosoxuc <ioseb.khutsishvili@gmail.com>
2023-02-27 13:13:57 -0700

    Add pre-commit hook

--- api/routers/buckets.py
+++ api/routers/buckets.py
@@ -12,10 +12,7 @@ router = APIRouter(prefix="", tags=["buckets"])
 
 
 def get_owner_data(owner):
-    return {
-        "ID": owner,
-        "DisplayName": owner
-    }
+    return {"ID": owner, "DisplayName": owner}
 
 
 def get_owner_buckets(owner):
@@ -24,39 +21,35 @@ def get_owner_buckets(owner):
 
 # List all buckets
 @router.get("/")
-def list_buckets(owner: str = Header(alias="x-amz-security-token"), ):
-    data = {
-        "Owner": get_owner_data(owner),
-        "Buckets": get_owner_buckets(owner)
-    }
+def list_buckets(
+    owner: str = Header(alias="x-amz-security-token"),
+):
+    data = {"Owner": get_owner_data(owner), "Buckets": get_owner_buckets(owner)}
     content = dicttoxml(data, attr_type=False, custom_root="ListAllMyBucketsResult")
     return Response(content=content, media_type="application/xml")
 
 
 @router.put("/{bucket}")
 def create_bucket(
-        bucket: str,
-        owner: str = Header(alias="x-amz-security-token"),
+    bucket: str,
+    owner: str = Header(alias="x-amz-security-token"),
 ):
     logging.warning(f"Creating bucket {bucket}")
-    MONGODB.buckets.insert_one({
-        "_id": bucket,
-        "Name": bucket,
-        "Owner": owner,
-        "CreationDate": datetime.now()
-    })
+    MONGODB.buckets.insert_one({"_id": bucket, "Name": bucket, "Owner": owner, "CreationDate": datetime.now()})
     content = dicttoxml({}, attr_type=False, custom_root="CreateBucketConfiguration")
     return Response(content=content, media_type="application/xml")
 
 
 @router.delete("/{bucket}")
 def delete_bucket(
-        bucket: str,
-        owner: str = Header(alias="x-amz-security-token"),
+    bucket: str,
+    owner: str = Header(alias="x-amz-security-token"),
 ):
     logging.warning(f"Deleting bucket {bucket}")
-    MONGODB.buckets.delete_one({
-        "_id": bucket,
-        "Owner": owner,
-    })
+    MONGODB.buckets.delete_one(
+        {
+            "_id": bucket,
+            "Owner": owner,
+        }
+    )
     return Response(status_code=204)
--- api/routers/objects.py
+++ api/routers/objects.py
@@ -20,54 +20,59 @@ def get_owner_objects(bucket, owner, prefix=None):
     }
     if prefix:
         query["Key"] = {"$regex": f"^{prefix}"}
-    yield from MONGODB.objects.find(query,
-                                    {"_id": 0, "Content": 0})
+    yield from MONGODB.objects.find(query, {"_id": 0, "Content": 0})
 
 
 @router.put("/{bucket}/{key}")
 async def create_object(
-        bucket: str,
-        key: str,
-        request: Request,
-        owner: str = Header(alias="x-amz-security-token"),
+    bucket: str,
+    key: str,
+    request: Request,
+    owner: str = Header(alias="x-amz-security-token"),
 ):
     logging.warning(f"Creating object {bucket}/{key}")
     content = await request.body()
-    MONGODB.objects.insert_one({
-        "_id": {"Bucket": bucket, "Key": key},
-        "Bucket": bucket,
-        "Key": key,
-        "Owner": owner,
-        "CreationDate": datetime.now(),
-        "Content": content
-    })
+    MONGODB.objects.insert_one(
+        {
+            "_id": {"Bucket": bucket, "Key": key},
+            "Bucket": bucket,
+            "Key": key,
+            "Owner": owner,
+            "CreationDate": datetime.now(),
+            "Content": content,
+        }
+    )
     content = dicttoxml({}, attr_type=False, custom_root="CreateBucketConfiguration")
     return Response(content=content, media_type="application/xml")
 
 
 @router.get("/{bucket}/{key}")
 async def get_object(
-        bucket: str,
-        key: str,
-        owner: str = Header(alias="x-amz-security-token"),
+    bucket: str,
+    key: str,
+    owner: str = Header(alias="x-amz-security-token"),
 ):
-    data = MONGODB.objects.find_one({
-        "_id": {"Bucket": bucket, "Key": key},
-        "Owner": owner,
-    })
+    data = MONGODB.objects.find_one(
+        {
+            "_id": {"Bucket": bucket, "Key": key},
+            "Owner": owner,
+        }
+    )
     return Response(content=data["Content"], media_type="application/octet-stream")
 
 
 @router.head("/{bucket}/{key}")
 async def head_object(
-        bucket: str,
-        key: str,
-        owner: str = Header(alias="x-amz-security-token"),
+    bucket: str,
+    key: str,
+    owner: str = Header(alias="x-amz-security-token"),
 ):
-    data = MONGODB.objects.find_one({
-        "_id": {"Bucket": bucket, "Key": key},
-        "Owner": owner,
-    })
+    data = MONGODB.objects.find_one(
+        {
+            "_id": {"Bucket": bucket, "Key": key},
+            "Owner": owner,
+        }
+    )
     if data:
         return Response(status_code=200)
     return Response(status_code=404)
@@ -75,30 +80,33 @@ async def head_object(
 
 @router.delete("/{bucket}/{key}")
 async def delete_object(
-        bucket: str,
-        key: str,
-        owner: str = Header(alias="x-amz-security-token"),
+    bucket: str,
+    key: str,
+    owner: str = Header(alias="x-amz-security-token"),
 ):
-    MONGODB.objects.delete_one({
-        "_id": {"Bucket": bucket, "Key": key},
-        "Owner": owner,
-    })
+    MONGODB.objects.delete_one(
+        {
+            "_id": {"Bucket": bucket, "Key": key},
+            "Owner": owner,
+        }
+    )
     return Response(status_code=204)
 
 
 @router.get("/{bucket}")
 async def list_objects(
-        bucket: str,
-        prefix: str = None,
-        owner: str = Header(alias="x-amz-security-token"),
+    bucket: str,
+    prefix: str = None,
+    owner: str = Header(alias="x-amz-security-token"),
 ):
     data = get_owner_objects(bucket, owner, prefix=prefix)
     root = minidom.Document()
-    xml = root.createElement('ListBucketResult')
+    xml = root.createElement("ListBucketResult")
     root.appendChild(xml)
     for obj in data:
-        xml.appendChild(root.createElement('Contents'))
-        xml.lastChild.appendChild(root.createElement('Key')).appendChild(root.createTextNode(obj["Key"]))
-        xml.lastChild.appendChild(root.createElement('LastModified')).appendChild(
-            root.createTextNode(obj["CreationDate"].isoformat()))
+        xml.appendChild(root.createElement("Contents"))
+        xml.lastChild.appendChild(root.createElement("Key")).appendChild(root.createTextNode(obj["Key"]))
+        xml.lastChild.appendChild(root.createElement("LastModified")).appendChild(
+            root.createTextNode(obj["CreationDate"].isoformat())
+        )
     return Response(content=root.toprettyxml(), media_type="application/octet-stream")
