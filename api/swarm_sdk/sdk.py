a85fb34a0978783c42d8bdaeb5a0c2e9b06afdc6
sosoxuc <ioseb.khutsishvili@gmail.com>
2023-02-27 14:56:33 -0700

    SWARM SDK for Python

---
+++ api/swarm_sdk/sdk.py
@@ -0,0 +1,36 @@
+import mimetypes
+from urllib.parse import urljoin
+
+import requests
+
+
+class SwarmClient:
+    def __init__(self, batch_id: str = None, server_url: str = "http://localhost:1633/"):
+        self.batch_id = batch_id
+        self.server_url = server_url
+
+    def generate_api_url(self):
+        url = self.server_url
+        if not url.endswith("/bzz"):
+            url = urljoin(url, "bzz")
+        return url
+
+    def download(self, file_id: str):
+        file_url = f"{self.generate_api_url()}/{file_id}/"
+        response = requests.get(file_url)
+        response.raise_for_status()
+        return response.content
+
+    def upload(self, file: str, name: str = None, content_type: str = None):
+        headers = {}
+        assert self.batch_id
+        headers["Swarm-Postage-Batch-Id"] = self.batch_id
+        file_obj = None
+        if isinstance(file, str):
+            file_obj = open(file, "rb")
+        content_type = content_type or mimetypes.guess_type(file)[0]
+        headers["Content-Type"] = content_type
+        api_url = self.generate_api_url()
+        response = requests.post(api_url, params={"name": name}, headers=headers, data=file_obj)
+        response.raise_for_status()
+        return response.json()
---
+++ api/tests/test_sdk.py
@@ -0,0 +1,11 @@
+from swarm_sdk.sdk import SwarmClient
+
+
+def test_swarm_sdk():
+    swarm = SwarmClient(
+        batch_id="b2432619b1e74aed26d7174a0601fe60aac617307114295ff9f715b8b88a6aaa",
+        server_url="http://104.237.159.189:1633/bzz",
+    )
+
+    result = swarm.upload("/home/soso/Pictures/screenshot.png")
+    print(result)
