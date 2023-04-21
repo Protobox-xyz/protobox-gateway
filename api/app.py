9a9de5cc5b5a2a2929224c4fce631e1f07a626c9
sosoxuc <ioseb.khutsishvili@gmail.com>
2023-04-21 17:49:11 +0400

    change routes

--- api/app.py
+++ api/app.py
@@ -5,14 +5,11 @@ from routers.buckets import router as buckets_router
 from routers.objects import router as objects_router
 from routers.health import router as health_router
 
-app = FastAPI()
+app = FastAPI(title="Protobox API", docs_url="/api/docs", redoc_url="/api/redoc")
 
-api = FastAPI(title="Protobox API")
-app.mount("/api", api)
-
-api.include_router(buckets_router)
-api.include_router(objects_router)
-api.include_router(health_router)
+app.include_router(buckets_router)
+app.include_router(objects_router)
+app.include_router(health_router)
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
--- api/settings.py
+++ api/settings.py
@@ -6,4 +6,4 @@ MONGODB_CONNECTION_STRING = os.environ.get("MONGODB_CONNECTION_STRING", "mongodb
 MONGODB_CLIENT = MongoClient(MONGODB_CONNECTION_STRING)
 MONGODB = MONGODB_CLIENT.protobox
 
-SWARM_SERVER_URL = os.environ.get("SWARM_SERVER_URL", "http://app.protobox.xyz:1633/")
+SWARM_SERVER_URL = os.environ.get("SWARM_SERVER_URL", "http://localhost:1633/")
