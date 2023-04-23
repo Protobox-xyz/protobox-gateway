3ab9c31bb2f37043fb3fd0c76f659226a88b130f
sosoxuc <ioseb.khutsishvili@gmail.com>
2023-04-23 10:32:27 +0400

    Log 422 messages

--- api/app.py
+++ api/app.py
@@ -1,5 +1,11 @@
+import logging
+
 import uvicorn
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from starlette import status
+from starlette.requests import Request
+from starlette.responses import JSONResponse
 
 from routers.buckets import router as buckets_router
 from routers.objects import router as objects_router
@@ -10,6 +16,15 @@ app = FastAPI(title="Protobox API", docs_url="/api/docs", openapi_url="/api/open
 app.include_router(buckets_router)
 app.include_router(objects_router)
 app.include_router(health_router)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    exc_str = f"{exc}".replace("\n", " ").replace("   ", " ")
+    logging.error(f"{request}: {exc_str}")
+    content = {"message": "Validation Error", "errors": exc.errors()}
+    return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
