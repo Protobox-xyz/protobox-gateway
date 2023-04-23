a4931fe3900859151e625a153195d5697af7c03b
sosoxuc <ioseb.khutsishvili@gmail.com>
2023-04-23 12:17:30 +0400

    Support file replece

--- api/routers/objects.py
+++ api/routers/objects.py
@@ -37,7 +37,8 @@ async def create_object(
     swarm_client = SwarmClient(batch_id=owner, server_url=SWARM_SERVER_URL)
     swarm_upload_data = swarm_client.upload(content, content_type=content_type, name=key)
     swarm_upload_data["SwarmServerUrl"] = SWARM_SERVER_URL
-    MONGODB.objects.insert_one(
+    MONGODB.objects.replace_one(
+        {"_id": {"Bucket": bucket, "Key": key}},
         {
             "_id": {"Bucket": bucket, "Key": key},
             "Bucket": bucket,
@@ -45,7 +46,8 @@ async def create_object(
             "Owner": owner,
             "CreationDate": datetime.now(),
             "SwarmData": swarm_upload_data,
-        }
+        },
+        upsert=True,
     )
     content = dicttoxml({}, attr_type=False, custom_root="CreateBucketConfiguration")
     return Response(content=content, media_type="application/xml")
