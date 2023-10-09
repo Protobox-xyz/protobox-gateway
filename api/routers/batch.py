ecd721dcf097219599f48f252e9673e2d73e377c
giorgi <gbagh16@freeuni.edu.ge>
2023-10-09 13:41:20 +0400

    security: get batches list for owner implemented

--- api/routers/batch.py
+++ api/routers/batch.py
@@ -15,7 +15,13 @@ async def handle_create_batch(owner: str = Depends(extract_signature)):
     return await create_batch(owner)
 
 
-@router.get("", response_model=BatchResponse | None)
+@router.get("/{batch_id}", response_model=BatchResponse | None)
 async def handle_get_batch(batch_id: str):
-    logging.warning(f"getting batch id {batch_id}")
+    logging.info(f"getting batch id {batch_id}")
     return MONGODB.batches.find_one({"batch_id": batch_id})
+
+
+@router.get("", response_model=list[BatchResponse])
+async def handle_get_batch(owner_address: str):
+    logging.info(f"getting batches {owner_address}")
+    return list(MONGODB.batches.find({"owner": owner_address}))
