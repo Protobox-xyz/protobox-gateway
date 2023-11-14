f70e36f16f2c0c1aa427b1121ec9c2d497657b60
giorgi <gbagh16@freeuni.edu.ge>
2023-11-14 12:54:05 +0300

    CLI: add authorization command

--- cli/main.py
+++ cli/main.py
@@ -5,34 +5,57 @@ import argparse
 import boto3
 import os
 
+import botocore
+
 ENDPOINT_URL = "http://localhost:8000/"
 BATCH_ID = "b00b92417dd7d5c379dba70afd4f986675e05df2635beab6ff2a6eb6eb8b2208"
-TOKEN = "258f929b1fa671db4009fe18646fce39e4a811f0f58bb4557e21121cc3d7f3a5/0x18a22dc629c07c02cbba3942ce3b977db76d23bc08e722eb6be42a5b6d6d7a8d02e271a6e88895a8662d5339d33efc8ca442299a01e5b5becc3145b5f01eea5c1c"
 
 
-# test123
+async def extract_token():
+    with open(f"{Path.home()}/.protobox/authorization.txt", "r") as file:
+        token = file.read()
+        return token
+
+
 async def get_client():
+    token = await extract_token()
     session = boto3.session.Session()
 
     return session.client(
         "s3",
         endpoint_url=ENDPOINT_URL,
-        aws_session_token=TOKEN,
-        aws_secret_access_key=TOKEN,
-        aws_access_key_id=TOKEN,
+        aws_session_token=token,
+        aws_secret_access_key=token,
+        aws_access_key_id=token,
     )
+
+
+async def handle_authorize(batch_id: str, signature: str):
+    token = f"{batch_id}/{signature}"
+    Path(f"{Path.home()}/.protobox").mkdir(parents=True, exist_ok=True)
+    with open(f"{Path.home()}/.protobox/authorization.txt", "w") as file:
+        file.write(token)
+
+    print(f"Credentials was saved in {Path.home()}/.protobox")
 
 
 async def handle_bucket_ls(bucket_name: str):
     client = await get_client()
-    print(client.list_objects(Bucket=bucket_name))
+
+    try:
+        print(client.list_objects(Bucket=bucket_name))
+    except botocore.exceptions.ClientError as e:
+        print(e)
 
 
 async def handle_download_object(bucket_name: str, key: str, dst_folder: str):
     client = await get_client()
-    data = client.get_object(Bucket=bucket_name, Key=key)
-    file_path = os.path.join(dst_folder, key)
+    try:
+        data = client.get_object(Bucket=bucket_name, Key=key)
+    except botocore.exceptions.ClientError as e:
+        print(e)
 
+    file_path = os.path.join(dst_folder, key)
     # Get the directory of the file
     directory = os.path.dirname(file_path)
     Path(directory).mkdir(parents=True, exist_ok=True)
@@ -48,7 +71,10 @@ async def handle_upload_folder(path: str, bucket_name: str):
             file_path = os.path.abspath(os.path.join(root, f))
             with open(file_path, "rb") as file:
                 bytes_data = file.read()
-                client.put_object(Bucket=bucket_name, Key=key, Body=bytes_data)
+                try:
+                    client.put_object(Bucket=bucket_name, Key=key, Body=bytes_data)
+                except botocore.exceptions.ClientError as e:
+                    print(e)
 
 
 async def main():
@@ -76,6 +102,14 @@ async def main():
     upload_folder.add_argument("-dir", "--directory", help="local folder or file path", default="")
     upload_folder.set_defaults(which="upload_folder")
 
+    # authorize command
+    authorize = subparses.add_parser("authorize", help="authorize command")
+    authorize.add_argument("-b", "--batch", help="batch id of the swarm", required=True)
+    authorize.add_argument(
+        "-sig", "--signature", help="specific signature generated to authorize on protobox", required=True
+    )
+    authorize.set_defaults(which="authorize")
+
     args = parser.parse_args()
     if args.which == "ls":
         await handle_bucket_ls(bucket_name=args.bucket)
@@ -83,6 +117,8 @@ async def main():
         await handle_download_object(bucket_name=args.bucket, key=args.key, dst_folder=args.destination)
     elif args.which == "upload_folder":
         await handle_upload_folder(bucket_name=args.bucket, path=args.directory)
+    elif args.which == "authorize":
+        await handle_authorize(batch_id=args.batch, signature=args.signature)
 
 
 if __name__ == "__main__":
