6f42265f4cbdea161eecf1ea0054404f7e2bc0ea
giorgi <gbagh16@freeuni.edu.ge>
2023-12-08 12:24:46 +0400

    superset: add embed functionality

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -2,3 +2,13 @@ SECRET_KEY = "M33X5m&1A[n9ilf]gvgz9,R*.a~&$&.}FJ(Y"
 PUBLIC_ROLE_LIKE_GAMMA = True
 AUTH_ROLE_PUBLIC = "Public"
 SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://superset:superset@postgres:5432/superset"
+FEATURE_FLAGS = {"EMBEDDED_SUPERSET": True}
+SESSION_COOKIE_SAMESITE = None
+ENABLE_PROXY_FIX = True
+CORS_OPTIONS = {
+    "supports_credentials": True,
+    "allow_headers": ["*"],
+    "resources": ["*"],
+    "origins": ["http://localhost:8088", "http://localhost:8888"],
+}
+ENABLE_JAVASCRIPT_CONTROLS = True
