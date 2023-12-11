1301460c338e6a51615f24ca395b8fa7c4153368
sosoxuc <ioseb.khutsishvili@gmail.com>
2023-12-11 22:34:43 +0400

    Remove scrollbars

--- stats/src/App.js
+++ stats/src/App.js
@@ -30,6 +30,7 @@ function App() {
     })
     const container = document.getElementById("my-superset-container");
     container.children[0].width = "100%";
+    container.children[0].scrolling = "no";
     container.children[0].height = "1000px";
   }, []);
 
